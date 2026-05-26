933a67d651c8ef765114eeabb9f871ae0b7190f0
husainshabu10-code <husainshabu10@gmail.com>
2026-05-26 19:07:11 +0530

    Simplify task row edit action

--- src/components/EventPrepDashboard.tsx
+++ src/components/EventPrepDashboard.tsx
@@ -1209,7 +1209,7 @@ function LiveTasksConfigTable({
                     <span>Priority</span>
                     <span>Due</span>
                     <span>Progress</span>
-                    <span>Edit</span>
+                    <span aria-label="Edit task" />
                   </div>
                   {tasks.map((task) => (
                     <TaskSummaryRow key={task.id} state={state} task={task} update={latestUpdates.get(task.id)} openTask={openTask} />
@@ -1252,7 +1252,7 @@ function TaskSummaryRow({ state, task, update, openTask }: { state: EventPrepSta
         </span>
         <strong>{progress}%</strong>
       </span>
-      <span className="task-summary-edit">Edit</span>
+      <span className="task-summary-edit" aria-hidden="true">Open</span>
     </button>
   );
 }
